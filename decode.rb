ca7b8efc5c5787b21dcfe7d6babc85541a22b2f1
Hammed Adisa <91697183+AbuRayhaan@users.noreply.github.com>
2022-07-14 19:45:28 +0100

    Modified the decode.rb file

--- decode.rb
+++ decode.rb
@@ -1,39 +1,26 @@
-def decode_alphabet(alphabet)
-    morse_codes = {
-        'A' => '.-',
-        'B' => '-...',
-        'C' => '-.-.',
-        'D' => '-..',
-        'E' => '.',
-        'F' => '..-.',
-        'G' => '--.',
-        'H' => '....',
-        'I' => '..',
-        'J' => '.---',
-        'K' => '-.-',
-        'L' => '.-..',
-        'M' => '--',
-        'N' => '-.',
-        'O' => '---',
-        'P' => '.--.',
-        'Q' => '--.-',
-        'R' => '.-.',
-        'S' => '...',
-        'T' => '-',
-        'U' => '..-',
-        'V' => '...-',
-        'W' => '.--',
-        'X' => '-..-',
-        'Y' => '-.--',
-        'Z' => '--..',
-    }
-    morse_codes.key(alphabet)
+def decode_char(char)
+  morse_codes = {
+    'A' => '.-', 'B' => '-...',
+    'C' => '-.-.', 'D' => '-..',
+    'E' => '.', 'F' => '..-.',
+    'G' => '--.', 'H' => '....',
+    'I' => '..', 'J' => '.---',
+    'K' => '-.-', 'L' => '.-..',
+    'M' => '--', 'N' => '-.',
+    'O' => '---', 'P' => '.--.',
+    'Q' => '--.-', 'R' => '.-.',
+    'S' => '...', 'T' => '-',
+    'U' => '..-', 'V' => '...-',
+    'W' => '.--', 'X' => '-..-',
+    'Y' => '-.--', 'Z' => '--..'
+  }
+  morse_codes.key(char)
 end
 
 def decode_word(word)
   array = word.split
   wordarray = []
-  array.each { |letter| wordarray.push(decode_alphabet(letter)) }
+  array.each { |letter| wordarray.push(decode_char(letter)) }
   wordarray.join
 end
 
